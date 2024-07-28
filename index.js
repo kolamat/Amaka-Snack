9c742f9506e91f9b405b433715ed74f670fc9925
kolamat <matanmidupe004@gmail.com>
2024-07-29 00:09:33 +0100

    On branch main Your branch is ahead of 'origin/main' by 1 commit.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // alert("you are my love");
 
-// let name = document.getElementById;
+let body = document.querySelector("body");
+body.style.margin = "0px";
+body.style.padding = "0px";
+body.style.boxSizing = "border-box";
 
 let ama = document.querySelector("h1");
 ama.textContent = "Chiamaka Night Food";
@@ -8,3 +11,4 @@ ama.style.display = "flex";
 ama.style.color = "pink";
 ama.style.justifyContent = "center";
 ama.style.fontSize = "50px";
+ama.style.marginTop = "0px";
