8e5114e522e733c1d79c0f7e254f2f74450d470a
kolamat <matanmidupe004@gmail.com>
2024-07-29 00:04:13 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   images/bana1.jpg
    	new file:   images/bana2.webp
    	new file:   images/cake1.jpeg
    	new file:   images/donut1.jpeg
    	modified:   index.js

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ let ama = document.querySelector("h1");
 ama.textContent = "Chiamaka Night Food";
 ama.style.display = "flex";
 ama.style.color = "pink";
-ama.style.alignItems = "center";
+ama.style.justifyContent = "center";
+ama.style.fontSize = "50px";
