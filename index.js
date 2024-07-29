1a4404d50c91d7eb1a0a8b66ed0a1ee24f3c626e
kolamat <matanmidupe004@gmail.com>
2024-07-29 18:23:07 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   index.js

--- index.js
+++ index.js
@@ -4,15 +4,21 @@ let body = document.querySelector("body");
 body.style.margin = "0px";
 body.style.padding = "0px";
 body.style.boxSizing = "border-box";
-body.style.display = "grid";
+body.style.display = "flex";
+body.style.flexDirection = "Column";
 body.style.alignItems = "center";
+body.style.overflowX = "hidden";
+
+// body.style.display = "flex";
+// div.style.justifyContent = "space-between";
 
 let div = document.querySelector("div");
-div.style.display = "gird";
-div.style.gridTemplateColumns = "100px 100px 100px";
+// div.style.display = "gird";
+// div.style.gridTemplateColumns = "100px 100px 100px";
 div.style.gap = "50px";
-// div.style.padding = "0px 10px 0px 10px";
-div.style.margin = "0px 50px 0px 50px";
+div.style.textAlign = "center";
+body.style.padding = "0px 10px 0px 10px";
+// div.style.margin = "0px 50px 0px 50px";
 div.style.overflow = "hidden";
 
 let ama = document.querySelector("h1");
@@ -24,30 +30,38 @@ ama.style.fontSize = "20px";
 ama.style.marginTop = "0px";
 ama.style.marginBottom = "6px";
 
-let container = document.createElement("div");
-container.textContent = "I love you";
+let container = document.createElement("a");
+container.href =
+  "Contact for delivery https://wa.me/[+2348150672809] Thank you patronizing";
+container.textContent = "";
 container.style.display = "flex";
 container.style.justifyContent = "center";
+container.style.fontStyle = "italic";
+container.style.padding = "0px 5px 0px 5px";
+container.style.textAlign = "center";
+container.style.fontWeight = "bold";
+container.style.background = "#000";
+container.style.color = "#fff";
 document.querySelector("body").appendChild(container);
 
 let image = document.createElement("img");
 image.src = "images/bana1.jpg";
-image.style.width = "200px";
+image.style.width = "300px";
 image.style.objectFit = "cover";
 document.querySelector("div").appendChild(image);
 let image2 = document.createElement("img");
 image2.src = "images/cake1.jpeg";
-image2.style.width = "200px";
+image2.style.width = "300px";
 image2.style.objectFit = "cover";
 document.querySelector("div").appendChild(image2);
 let image3 = document.createElement("img");
 image3.src = "images/cupcake1.jpeg";
-image3.style.width = "200px";
+image3.style.width = "300px";
 image3.style.objectFit = "cover";
 document.querySelector("div").appendChild(image3);
 let image1 = document.createElement("img");
 image1.src = "images/donut1.jpeg";
-image1.style.width = "200px";
+image1.style.width = "300px";
 image1.style.objectFit = "cover";
 document.querySelector("div").appendChild(image1);
 
