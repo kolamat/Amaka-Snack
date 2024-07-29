82ab005f5e32cf00b022252f1df956d939b1bbaa
kolamat <matanmidupe004@gmail.com>
2024-07-29 19:05:30 +0100

    Your branch is ahead of 'origin/main' by 1 commit.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-// alert("you are my love");
+alert(
+  "This are for you but i dont which one you will like so, and also so sorry it came in late"
+);
 
 let body = document.querySelector("body");
 body.style.margin = "0px";
@@ -31,37 +33,43 @@ ama.style.marginTop = "0px";
 ama.style.marginBottom = "6px";
 
 let container = document.createElement("a");
-container.href = " https://wa.me/[+2348150672809] ";
-container.textContent = "Contact for delivery Thank you patronizing";
+container.href = " https://wa.me/+2348150672809";
+container.textContent =
+  "Contact for delivery Thank you patronizing Click : [+2348150672809]";
 container.style.display = "flex";
 container.style.justifyContent = "center";
-container.style.fontStyle = "italic";
+container.style.borderRadius = "3px 3px 8px 8px";
+container.style.fontStyle = "";
 container.style.padding = "0px 5px 0px 5px";
 container.style.textAlign = "center";
 container.style.fontWeight = "bold";
 container.style.background = "#000";
-container.style.color = "#fff";
+container.style.color = "#d1cbe8";
 document.querySelector("body").appendChild(container);
 
 let image = document.createElement("img");
 image.src = "images/bana1.jpg";
 image.style.width = "300px";
 image.style.objectFit = "cover";
+image.style.borderRadius = "10px";
 document.querySelector("div").appendChild(image);
 let image2 = document.createElement("img");
 image2.src = "images/cake1.jpeg";
 image2.style.width = "300px";
 image2.style.objectFit = "cover";
+image2.style.borderRadius = "10px";
 document.querySelector("div").appendChild(image2);
 let image3 = document.createElement("img");
 image3.src = "images/cupcake1.jpeg";
 image3.style.width = "300px";
 image3.style.objectFit = "cover";
+image3.style.borderRadius = "10px";
 document.querySelector("div").appendChild(image3);
 let image1 = document.createElement("img");
 image1.src = "images/donut1.jpeg";
 image1.style.width = "300px";
 image1.style.objectFit = "cover";
+image1.style.borderRadius = "10px";
 document.querySelector("div").appendChild(image1);
 
 // let image1 = document.createElement("img");
